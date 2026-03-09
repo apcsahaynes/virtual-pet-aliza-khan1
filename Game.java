af2fcb318de4d473d1fa632816e02b60d10d0d72
aliza-khan1 <aliza.khan@jpschools.org>
2026-03-08 22:25:04 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,3 +1,37 @@
 public class Game {
+    private String name;
+    public int happinessIncr;
+    public int weightDecr;
     
+  public Game(String name, int happinessIncr, int weightDecr){
+    this.name = name;
+    this.happinessIncr = happinessIncr;
+    this.weightDecr = weightDecr;
+  }
+  
+  public String getName(){
+    return name;
+  }
+  
+  public int getHappinessIncr(){
+    return happinessIncr;
+  }
+  
+  public int getWeightDecr(){
+    return weightDecr;
+  }
+   
+ // public boolean play(Game g){
+  //    return true;
+  //}
+  public boolean isWinner(){
+      Math.random();
+      if(Math.random()>0.5){
+          return true;
+      }
+      else{
+          return false;
+      }
+  }
+ 
 }
