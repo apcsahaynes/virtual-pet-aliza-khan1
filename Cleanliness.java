02cd3c175b303be967571ebbef8f6d81022f2bb0
aliza-khan1 <aliza.khan@jpschools.org>
2026-03-08 22:24:27 -0500

    Create Cleanliness.java

---
+++ Cleanliness.java
@@ -0,0 +1,20 @@
+public class Cleanliness {
+    private String name;
+    public int cleanlinessIncrease;
+    
+  public Cleanliness(String name, int cleanlinessIncrease){
+    this.name = name;
+    this.cleanlinessIncrease = cleanlinessIncrease;
+  }
+  
+  public String getName(){
+    return name;
+  }
+  
+  public int getcleanlinessIncrease(){
+    return cleanlinessIncrease;
+  }
+    
+    
+    
+}
